5ab72a2851573e626081f40f05693aee706a427b
RakibRahman <md.rr.talukder@gmail.com>
2021-09-28 08:19:33 +0600

    Prettier and Eslint updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import { AppContainer } from "./styles";
+import { AppContainer } from './styles';
 
 export const App: React.FC = ({ children }) => {
-  return (
-    <AppContainer className="App">
-      <h1 className="App-header">Hello TypeScript!</h1>
-      <p>Hello</p>
-    </AppContainer>
-  );
+    return (
+        <AppContainer className="App">
+            <h1 className="App-header">Hello TypeScript!</h1>
+            <p>Hello</p>
+        </AppContainer>
+    );
 };
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,15 +1,15 @@
-import { ColumnContainer, ColumnTitle } from "./styles";
-import { FC } from "react";
+import { ColumnContainer, ColumnTitle } from './styles';
+import { FC } from 'react';
 type ColumnProps = {
-  text: string;
+    text: string;
 };
 //! React.FC type to define the children prop on component.
 
 export const Column: FC<ColumnProps> = ({ text, children }) => {
-  return (
-    <ColumnContainer>
-      <ColumnTitle>{text}</ColumnTitle>
-      {children}
-    </ColumnContainer>
-  );
+    return (
+        <ColumnContainer>
+            <ColumnTitle>{text}</ColumnTitle>
+            {children}
+        </ColumnContainer>
+    );
 };
